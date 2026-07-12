fe93b2971bbea08f4cfc521fe78424a42a6029c5
Yana <julianaaslor1212@gmail.com>
2026-07-13 01:46:49 +0800

    Build contact section

--- src/main.js
+++ src/main.js
@@ -5,4 +5,5 @@ import "../styles/components/hero.css";
 import "../styles/components/about.css";
 import "../styles/components/featured.css";
 import "../styles/components/projects.css";
+import "../styles/components/contact.css";
 import "../styles/utils.css";
