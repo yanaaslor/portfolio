88d38c47f0c451b327bb2df7769efc01d3c65e1e
Yana <julianaaslor1212@gmail.com>
2026-07-14 18:37:50 +0800

    Add blur animation to lazy loading

--- src/utils/lazy-loading.js
+++ src/utils/lazy-loading.js
@@ -6,6 +6,8 @@ const lazyLoading = () => {
       if (entry.isIntersecting) {
         let img = entry.target;
         img.src = img.dataset.src;
+        img.classList.remove("loading");
+        img.classList.add("loaded");
         observer.unobserve(img);
       }
     });
